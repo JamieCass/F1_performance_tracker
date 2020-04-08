bd9fa1f01fed4d56409e227c60bf6676a19002d4
Jamie <jamie@Jamies-MacBook.modem>
2020-04-08 21:57:33 +1000

    played with trends

---
+++ google_trends.py
@@ -0,0 +1,16 @@
+GOOGLE TRENDS HELP!!!
+
+# Google trends help.
+
+import pytrends
+from pytrends.request import TrendReq
+
+pytrend = TrendReq() # this connects to google. 
+
+google hot trends data, daily search trends..
+pytrend.trending_searches(pn = 'australia') (could put it in a dataframe)
+
+worldwide, you wouldnt put a parameter. so get rid of 'pn'
+
+todays trending search..
+pytrend.today_searches
