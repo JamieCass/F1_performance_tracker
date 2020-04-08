a274fddd65000e13a0a7da3aa3c2781ac8e291d2
Jamie <jamie@Jamies-MacBook.modem>
2020-04-08 22:47:30 +1000

    sophie comment

--- Trends.py
+++ Trends.py
@@ -21,4 +21,5 @@ world = pytrend.build_payload(kw_list=['Lewis Hamilton'])
 dd = pytrend.interest_by_region()
 dd.reset_index().plot(x='geoName', y='Lewis Hamilton', figsize=(120, 10), kind='bar')
 
-print('hello world')
+
+print('hello sophie')
