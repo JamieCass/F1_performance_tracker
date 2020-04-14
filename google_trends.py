c96395b1df4cbf2023fc3741bf20a097402509ce
Jamie <jamie@Jamies-MacBook.modem>
2020-04-14 17:48:25 +1000

    few extra bits added to notes

--- google_trends.py
+++ google_trends.py
@@ -2,15 +2,27 @@ GOOGLE TRENDS HELP!!!
 
 # Google trends help.
 
+# ALl help came from https://github.com/Tanu-N-Prabhu/Python/blob/master/Google_Trends_API.ipynb
+
 import pytrends
 from pytrends.request import TrendReq
+import pandas as pd
 
-pytrend = TrendReq() # this connects to google. 
+pytrend = TrendReq() # this connects to google.
 
-google hot trends data, daily search trends..
-pytrend.trending_searches(pn = 'australia') (could put it in a dataframe)
+# google hot trends data, daily search trends..
+pytrend.trending_searches(pn = 'australia')
 
-worldwide, you wouldnt put a parameter. so get rid of 'pn'
+# worldwide, you wouldnt put a parameter. so get rid of 'pn'
 
-todays trending search..
+# todays trending search..
 pytrend.today_searches
+
+df = pytrend.trending_searches(pn='united_kingdom')
+df
+
+
+# google keywords suggestions.
+keywords = pytrend.suggestions(keyword = 'Mercedes benz')
+df_1 = pd.DataFrame(keywords)
+df_1.drop(columns = 'mid') # mid column didnt make sense..
--- google_trends_tracker.py
+++ google_trends_tracker.py
@@ -8,3 +8,6 @@ pytrend.build_payload(kw_list=['Lewis Hamilton'])
 # Interest by Region
 df = pytrend.interest_by_region()
 df.head(10)
+
+# bar chart for all geoNames and searches.
+df.reset_index().plot(x='geoName', y='Lewis Hamilton', figsize=(120, 10), kind='bar')
