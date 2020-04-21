e3a2e5fdcf2b8fedf6a2c241bf85691fc75dfa46
Jamie <jamiecass92@gmail.com>
2020-04-21 18:31:59 +1000

    added DATE and a few other bits

--- google_trends_tracker.py
+++ google_trends_tracker.py
@@ -2,10 +2,70 @@ import utils
 import pytrends
 import pandas as pd
 import csv
-
 from pytrends.request import TrendReq
+import time
+import datetime
+# from pytrends.request import TrendReq
+# pytrend = TrendReq()
+# pytrend.build_payload(kw_list=['Lewis Hamilton','Daniel Ricardo','Sebastian Vettel','Charles Leclerc','Max Verstappen'])
+
+DATE = datetime.datetime.fromtimestamp(time.time()).strftime('%d-%m-%y')
+
+DATE
+
 pytrend = TrendReq()
-pytrend.build_payload(kw_list=['Lewis Hamilton','Daniel Ricardo','Sebastian Vettel','Charles Leclerc','Max Verstappen'])
+driver_list = ['Lewis Hamilton','Valtteri Bottas','Sebastian Vettel','Charles Leclerc','Pierre Gasly','Max Verstappen','Daniel Ricardo','Nico Hulkenberg','Romain Grosjean','Kevin Magnussen','Lando Norris','Carlos Sainz','Sergio Perez','Lance Stroll','Kimi Raikkonen','Antonio Giovinazzi','Alexander Albon','Daniil Kvyat','George Russel','Robert Kubica']
+total_drivers = len(driver_list)
+# Number of times we need to loop over
+max_google_request = 5
+iter = int(total_drivers/max_google_request)
+# Difference we need to add every time
+diff = int(total_drivers/iter)
+# Set an empty df
+full_results_region = pd.DataFrame()
+counter = 0
+for i in range(iter):
+    iter_drivers = driver_list[counter:counter+diff]
+    print('COMPLETE:',print(iter_drivers))
+    counter+=diff
+    pytrend.build_payload(kw_list=iter_drivers)
+    df_region = pytrend.interest_by_region()
+    ## Can add other things to the payload
+    #df_interest_over_time = pytrend.interest_over_time()
+    # APPEND TO FULL RESULTS
+    full_results_region = full_results_region.append(df.T)
+    full_results_region['datetime'] = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d')
+    print('full_results len',len(full_results_region))
+    # CHECK
+    print('COMPLETE:',print(iter_drivers))
+    # SAVE
+    #full_results_region.to_csv('full_results_region.csv',index=False)
+
+datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d')
+
+
+
+iter_drivers
+
+
+
+
+full_results_overtime = full_results_overtime.append(df_interest_over_time)
+df_interest_over_time.drop(['isPartial'], axis=1)
+full_results_region
+
+
+full_results_overtime
+
+
+datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d')
+
+
+
+teamtrend = TrendReq()
+teamtrend.build_payload(kw_list=['Mercedes','Ferrari','Red Bull','Renault'])
 # Interest by Region
 df = pytrend.interest_by_region()
 df.head(10)
@@ -13,6 +73,8 @@ df.head(10)
 # bar chart for all geoNames and searches.
 df.reset_index().plot(x='geoName', y='Lewis Hamilton', figsize=(120, 10), kind='bar')
 
+construstors=teamtrend.interest_by_region()
+construstors.plot(figsize=(20,10))
 # How many countries search for Lewis Hamilton?
 
 df[df['Lewis Hamilton']>0]
@@ -85,6 +147,8 @@ df.sum()
 dir(pytrend)
 
 # look at interest over time between all 5 drivers.
+pytrend.build_payload(kw_list=, timeframe='now 7-d')
+
 df_overtime = pytrend.interest_over_time()
 df_overtime
 
@@ -106,6 +170,7 @@ ham_max_overtime = df_overtime2018[['Max Verstappen','Lewis Hamilton']]
 
 #Graph showing just max and hammi searches in 2018
 ham_max_overtime.plot(figsize=(20,10))
+
 # max got searched a lot after getting 2nd at Brazil and also when he won at Austria.
 df_overtime2018.sort_values('Max Verstappen', ascending=False)
 # Plot into a graph
